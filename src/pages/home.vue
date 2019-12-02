76d727ba12850ccf5384caf4c9ab0494a0d21520
alexxalex <alxsar98@gmail.com>
2019-12-03 02:41:30 +0300

    reset

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,8 +2,6 @@
     <div class="row">
       <div class="col-12">
         <img src="../statics/img/backgrounggirl.jpg" style="width: 100%   ">
-        <img src="../statics/img/backgrounggirl.jpg" style="width: 100%   ">
-        <img src="../statics/img/backgrounggirl.jpg" style="width: 100%   ">
       </div>
     </div>
 </template>
