8c95d7d44e0738e24a345de40528080dda65ae0c
Alex <richyshark@gmail.com>
2019-12-04 22:44:52 +0300

    aaa

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,31 +1,105 @@
 <template>
-    <q-page class="q-py-md">
-        <div class="row">
-            <card v-for="article in articles.slice(0,2)" :article="article" :key="article.id"></card>
+    <q-page class="q-py-md q-ml-xl q-mr-xl q-mt-lg">
+      <q-carousel
+        arrows
+        animated
+        v-model="slide"
+        height="50vh"
+      >
+        <q-carousel-slide name="first" img-src="https://images.axios.com/gABDn8zrypC1FambOSaB6WZlfkg=/0x0:1920x1080/1920x1080/2019/08/21/1566383191315.jpg">
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2 text-white">Будь в тренде!</div>
+            <div class="text-subtitle1 text-white">Будь на хайпе.</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="second" img-src="https://wallpaperaccess.com/full/87243.jpg">
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2 text-white">Аниме клёво</div>
+            <div class="text-subtitle1 text-white">Если ты педик</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="third" img-src="https://cdn.wallpapersafari.com/15/38/YvwZPC.jpg">
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2 text-white">Вэйп эстетика</div>
+            <div class="text-subtitle1 text-white">So much clouds</div>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="fouth" img-src="http://sun9-48.userapi.com/c637717/v637717426/31bb9/zu_bCIUhYYY.jpg">
+          <div class="absolute-bottom custom-caption">
+            <div class="text-h2">Вэйп-нэйшн, брат</div>
+            <div class="text-subtitle1">Йоу бичез</div>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+      <container >
+        <div class="row q-mt-xl">
+          <div class="col-3"></div>
+          <div class="col-6">
+            <div class="container">
+              <div class="row items-center">
+                <div class="col-4">
+                  <img src="../assets/gl-1.png" width="70%"  alt="">
+                </div>
+                <div class="col-8 justify-start">
+                  <label class="text-h5"> <p>Просто уникальный магазин.</p>
+                    <p>Только в нашем магазине вы сможете встретить Семенович!</p>
+                    <p>Правда Лену, но ладно..</p></label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3"></div>
         </div>
+      </container>
+      <container >
         <div class="row">
-            <card size="small" v-for="article in articles.slice(2,5)" :article="article" :key="article.id"></card>
+          <div class="col-3"></div>
+          <div class="col-6">
+            <div class="container">
+              <div class="row items-center">
+                <div class="col-8 justify-end">
+                  <label class="text-h5"> Перейдя на вейп, вам больше не придётся тратиться на зажигалки!</label>
+                </div>
+                <div class="col-4">
+                  <img src="../assets/gl-2.png" width="90%"  alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3"></div>
         </div>
-        <div class="row">
-            <card v-for="article in articles.slice(5,7)" :article="article" :key="article.id"></card>
+      </container>
+      <container >
+        <div class="row q-mb-xl">
+          <div class="col-3"></div>
+          <div class="col-6">
+            <div class="container">
+              <div class="row items-center">
+                <div class="col-4">
+                  <img src="../assets/gl-3.png" width="100%"  alt="">
+                </div>
+                <div class="col-8 justify-start">
+                  <label class="text-h5"> Всегда рады вас видеть в нашем магазине!</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3"></div>
         </div>
-        <div class="row">
-            <card size="small" v-for="article in articles.slice(7,10)" :article="article" :key="article.id"></card>
-        </div>
+      </container>
     </q-page>
 </template>
 
 <script>
-import card from '../components/Card'
 import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'Home',
   components: {
-    card
   },
   data () {
     return {
-      articles: null
+      articles: null,
+      slide: 'first'
     }
   },
   computed: {
