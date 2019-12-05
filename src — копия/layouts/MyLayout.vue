f19eb42dd200a14cb1b518c3a936d85054226001
Alex <richyshark@gmail.com>
2019-12-05 19:10:07 +0300

    asasa

---
+++ src — копия/layouts/MyLayout.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-layout view="hhh lpR fff">
+      <q-header reveal elevated class="text-white bg-black q-px-md q-py-sm">
+      <q-toolbar v-if="width >= 992"  class="row">
+        <q-toolbar-title class="header-logo flex items-center">
+          <a href="/"> <img src="../assets/log.png" alt=""></a>
+          <q-btn to="/" class="q-ml-xl text-h6">ГЛАВНАЯ</q-btn>
+          <q-btn to="/catalog" class="q-ml-xl text-h6">КАТАЛОГ</q-btn>
+        </q-toolbar-title>
+        <q-list class=" text-h6 flex justify-between items-center">
+            <q-item class="text-white q-mr-xl" clickable to="/">
+              <q-item-section>
+                  <q-item-label>ВХОД</q-item-label>
+              </q-item-section>
+            </q-item>
+          <q-item class="text-white q-mr-xl" clickable to="/articles">
+            <q-item-section>
+              <img src="../assets/lk.png" alt="">
+            </q-item-section>
+          </q-item>
+          <q-item class="text-white" clickable to="/articles">
+            <q-item-section>
+              <img src="../assets/bucket.png" alt="">
+              <q-badge floating color="red">{{this.$store.state.quant}}</q-badge>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-toolbar>
+      <q-toolbar v-else class="row q-px-none">
+        <q-toolbar-title class="header-logo flex items-center q-px-none">
+          <img src="../assets/Logo.png" alt="">
+        </q-toolbar-title>
+        <q-btn  flat class="q-pa-sm" icon="menu">
+          <q-menu max-width="600" anchor="top right" self="bottom right">
+            <div class="row">
+              <div class="column">
+                <q-item class="text-grey-8 text-center block" clickable to="/">
+                <q-item-section>
+                  <q-item-label>Главная</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item class="text-white text-center block" clickable to="/article">
+                <q-item-section>
+                  <q-item-label>Статьи</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item class="text-white text-center block" clickable to="/categories">
+                <q-item-section>
+                  <q-item-label>Категории</q-item-label>
+                </q-item-section>
+              </q-item>
+              </div>
+
+              <q-separator inset vertical />
+
+              <div class="column justify-around items-center">
+                <q-btn class="block q-ma-sm" icon="search"/>
+                <q-btn class="block q-ma-sm" to="/login" color="primary" label="Вход" />
+                <q-btn class="block q-ma-sm" to="/register" color="amber" label="Регистрация" />
+              </div>
+            </div>
+          </q-menu>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer  elevated class="bg-black text-white">
+      <q-list class="row justify-between items-center">
+        <q-list class="row text-h6">
+          <q-item>
+            <q-item-label>+7925-567-24-00</q-item-label>
+          </q-item>
+          <q-item>
+            <q-item-label>ТЕЛЕФОНЫ И АДРЕСА</q-item-label>
+          </q-item>
+        </q-list>
+        <q-list class="flex justify-end items-center">
+          <q-item class="text-white justify-end" clickable to="/articles">
+            <q-item-section>
+              <img src="../assets/instagramm.png" alt="">
+            </q-item-section>
+          </q-item>
+          <q-item class="text-white justify-end" clickable to="/articles">
+            <q-item-section>
+              <img src="../assets/google.png" alt="">
+            </q-item-section>
+          </q-item>
+          <q-item class="text-white justify-end" clickable to="/articles">
+            <q-item-section>
+              <img src="../assets/vk.png" alt="">
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-list>
+    </q-footer>
+
+  </q-layout>
+</template>
+<script>
+export default {
+  name: 'he',
+  data () {
+    return {
+      text: '',
+      model: '',
+      width: 0
+    }
+  },
+  created () {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
+  },
+  methods: {
+    handleResize () {
+      this.width = window.innerWidth
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+  .header
+    &-logo
+      img
+        max-width: 221px
+  .block
+    min-width: 150px
+</style>
---
+++ src — копия/pages/catalog.vue
@@ -0,0 +1,188 @@
+<template>
+  <q-page class="q-py-lg q-px-md">
+    <div class="container q-py-lg">
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+          <q-card class="my-card text-h6 q-mb-md">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-5 col-sm-12 col-xs-12 col column">
+                  <q-card-section class="text-grey-8">
+                    <div class="row flex justify-between items-center">
+                      <div class="col-md-1 flex">{{label.min}}</div>
+                      <div class="col-md-10 col-sm-10 col-xs-12 q-pa-md">
+                        <q-range
+                          v-model="label"
+                          :min="350"
+                          :max="1000"
+                          :step="4"
+                          label
+                          color="primary"
+                        />
+                      </div>
+                      <div class="col-md-1 flex">{{label.max}}</div>
+                    </div>
+                  </q-card-section>
+                </div>
+                <div class="col-md-2 col-sm-4 col-xs-12 flex items-center justify-center">
+                  <q-card-section class="text-grey-8">Сортировать по: </q-card-section>
+                </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 q-mb-sm flex flex items-center justify-center">
+                    <q-btn outline color="accent" label="Рйтингу" />
+                </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 q-mb-sm flex flex items-center justify-center ">
+                      <q-btn outline color="accent" label="Цене" />
+                </div>
+              </div>
+            </div>
+          </q-card>
+        </div>
+        <div class="col-1"></div>
+        <div class="flex col-4 justify-center">
+        </div>
+        <div class="col-1"></div>
+      </div>
+      <div class="row " >
+        <div class="col-1"></div>
+        <div class="col-10 ">
+          <div class="row q-py-lg  justify-center  items-center">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+          <div class="row q-py-lg  justify-center  items-center">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 q-my-lg flex justify-center ">
+              <q-card class="my-card flex justify-center items-center" style="width: 250px; min-width: 200px; min-height: 300px ">
+                <img class="q-mt-sm" src="../statics/img/vape.png" style="max-height: 290px; width: auto">
+                <q-card-section>
+                  <div class="text-h6">Vandy Vape Swell-боксмод</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="row flex items-center justify-center ">
+                    <div class="col-12 text-h5 text-center  text-red-8">4490 руб./шт</div>
+                    <div class="col-12 flex q-pt-lg justify-center items-center"><q-btn outline color="accent" label="В корзину"/></div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+        </div>
+        <div class="col-1"></div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'catalog',
+  data () {
+    return {
+      label: {
+        min: 350,
+        max: 1000
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
