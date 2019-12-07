08ebb474f6b9c88dcda4cab066f9a48e7eb57aa7
Alex <richyshark@gmail.com>
2019-12-07 22:41:47 +0300

    pizdec

--- src/router/routes.js
+++ src/router/routes.js
@@ -27,6 +27,34 @@ const routes = [
       {
         path: 'input',
         component: () => import('pages/input.vue')
+      },
+      {
+        path: 'vape',
+        component: () => import('pages/vape.vue')
+      },
+      {
+        path: 'pod',
+        component: () => import('pages/pod.vue')
+      },
+      {
+        path: 'liquid',
+        component: () => import('pages/liquid.vue')
+      },
+      {
+        path: 'accessories',
+        component: () => import('pages/accessories.vue')
+      },
+      {
+        path: 'mod',
+        component: () => import('pages/mod.vue')
+      },
+      {
+        path: 'wliquid',
+        component: () => import('pages/wliquid.vue')
+      },
+      {
+        path: 'atom',
+        component: () => import('pages/atom.vue')
       }
     ]
   },
